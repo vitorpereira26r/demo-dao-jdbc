7437c66b38540a864017a68fc79536cff898fa0c
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-03-15 15:36:53 -0300

    Seller entity class

--- demo-jao-jdbc/src/Main.java
+++ demo-jao-jdbc/src/Main.java
@@ -1,9 +1,16 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dep1 = new Department(1, "Books");
-        System.out.println(dep1);
+
+        Seller seller1 = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2000.0, dep1);
+
+
+        System.out.println(seller1);
     }
 }
