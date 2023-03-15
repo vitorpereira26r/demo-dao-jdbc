f2fad75458d6b56a21eabe21aee6a698970d826a
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-03-15 16:29:11 -0300

    findByDepartment implementation

--- demo-jao-jdbc/src/Main.java
+++ demo-jao-jdbc/src/Main.java
@@ -1,6 +1,10 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +13,13 @@ public class Main {
 
         System.out.println("=== TEST 1 : seller findById =====");
         Seller seller1 = sellerDao.findById(3);
-
         System.out.println(seller1);
+
+        System.out.println("\n=== TEST 2 : seller findByDepartment =====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller s : list){
+            System.out.println(s);
+        }
     }
 }
